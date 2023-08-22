df2737de5098443ff3d1810be1adff0e82fa36eb
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-08-22 11:35:10 -0400

    added dependencies and organized directories and files.

---
+++ holiday/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo/holiday-logo.png' 
+import "./styles.css";
+
+function Header() {
+  return (
+    <div className='navbar'>
+        
+    </div>
+  )
+}
+
+export default Header;
